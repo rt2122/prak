33cb0a6f7b57c645eba95f7f2ec68ad6ca9ce818
Alisa Nemeshaeva <s02170175@stud.cs.msu.ru>
2018-11-30 11:49:22 +0300

    Add files via upload

---
+++ prak/11/4text.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <wait.h>
+
+volatile int flag = 0;
+
+void handler()
+{
+    flag = 1;
+}
+
+int main(int argc, char **argv)
+{
+    int max, num = 0;
+    sscanf(argv[1], "%d", &max);
+    sigset_t s1, s2;
+    sigemptyset(&s1);
+    sigaddset(&s1, SIGUSR1);
+    sigprocmask(SIG_BLOCK, &s1, &s2);
+    sigaction(SIGUSR1, &(struct sigaction){ .sa_handler = handler, .sa_flags = SA_RESTART }, NULL);
+    int fd[2];
+    pipe(fd);
+    FILE *r = fdopen(fd[0], "r");
+    FILE *w = fdopen(fd[1], "a");
+    int pid[2];
+    int pid1, pid2;
+    pid1 = fork();
+    if (pid1) {
+        pid2 = fork();
+        if (pid2) {
+            //int x = 1;
+            //write(fd[1], &x, sizeof(x));
+            //x = pid2;
+            //write(fd[1], &x, sizeof(x));
+            fprintf(w, "1 %d ", pid2);
+            fflush(w);
+            kill(pid1, SIGUSR1);
+            wait(NULL);
+            wait(NULL);
+            printf("Done\n");
+            _exit(0);
+        } else {
+            num = 2;
+        }
+    } else {
+        num = 1;
+    }
+
+    while (1) {
+        //while (!flag) {
+          //  sigprocmask(SIG_SETMASK, &s2, NULL);
+            //pause();
+            //sigprocmask(SIG_BLOCK, &s1, NULL);
+        //}
+
+        while (!flag) {
+            sigsuspend(&s2);
+        }
+
+        flag = 0;
+        int x, pid;
+        //read(fd[0], &x, sizeof(x));
+        //read(fd[0], &pid, sizeof(pid));
+        fscanf(r, "%d%d", &x, &pid);
+        if (x == max) {
+            _exit(0);
+        }
+        printf("%d %d\n", num, x);
+        ++x;
+        int mypid = getpid();
+        //num = 1 - num;
+        //write(fd[1], &x, sizeof(x));
+        //write(fd[1], &mypid, sizeof(mypid));
+        fprintf(w,"%d %d ", x, mypid);
+        fflush(w); 
+        kill(pid, SIGUSR1);
+        if (x == max) {
+            _exit(0);
+        }
+    }
+}
